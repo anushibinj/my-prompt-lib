cd80b91d89255224ff33fe4dd2e6d12bd1815adc
Anu Shibin Joseph Raj <anushibin007@gmail.com>
2026-04-01 11:15:25 +0530

    ✨ Firebase

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,28 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAPzxuWcq7BiB0XSGQn1l3hP0SRiRTujCE",
+  authDomain: "fastorial.firebaseapp.com",
+  projectId: "fastorial",
+  storageBucket: "fastorial.firebasestorage.app",
+  messagingSenderId: "1032492683738",
+  appId: "1:1032492683738:web:509ed55ebf5ea3ce5670e3",
+  measurementId: "G-W86QGMSP4D"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
